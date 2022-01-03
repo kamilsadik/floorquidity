9c0fe9a0e3e5edfcda2cbf3bd1e5a767689ed955
kamilsadik <kamilsadik@gmail.com>
2022-01-03 13:23:49 -0500

    Sketched out additional tests

--- test/LiquidityFactory.js
+++ test/LiquidityFactory.js
@@ -27,7 +27,7 @@ contract("LiquidityFactory", (accounts) => {
         // bidder is bidding 0 ETH for 1 BAYC
         await utils.shouldThrow(contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 0}));
     });
-    it("should be able to cancel bids previously submitted on a given collection", async () => {
+    it("should be able to cancel a single bid previously submitted on a given collection", async () => {
         // bidder is bidding 0.1 ETH for 1 BAYC
         await contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 100000000000000000});
         // bidder is canceling BAYC bid
@@ -39,6 +39,18 @@ contract("LiquidityFactory", (accounts) => {
         assert.equal(result.logs[0].args.quantity, 1);
         assert.equal(result.logs[0].args.tokenId, 0);
     });
+    it("should be able to cancel multiple bids previously submitted on a given collection", async () => {
+      // bidder is bidding 0.2 ETH for 2 BAYC
+      await contractInstance.submitBid(BAYC_ADDRESS, 2, {from: bidder, value: 200000000000000000});
+      // bidder is canceling BAYC bid
+      const result = await contractInstance.cancelBid(BAYC_ADDRESS, {from: bidder});
+      assert.equal(result.receipt.status, true);
+      assert.equal(result.logs[0].args.bidderAddress, bidder);
+      assert.equal(result.logs[0].args.nftAddress, BAYC_ADDRESS);
+      assert.equal(result.logs[0].args.weiPriceEach, 200000000000000000);
+      assert.equal(result.logs[0].args.quantity, 1);
+      assert.equal(result.logs[0].args.tokenId, 0);
+  });
     it("should not be able to place a bid on a collection where you already have a bid outstanding", async () => {
       // bidder is bidding 0.1 ETH for 1 BAYC
       await contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 100000000000000000});
@@ -65,8 +77,8 @@ contract("LiquidityFactory", (accounts) => {
     });
   })
 
-  context("as a seller, selling an NFT from an ERC-721 collection", async () => {
-    it("should be able to sell a single NFT into a bid for a single NFT", async () => {
+  xcontext("as a seller, selling an NFT from an ERC-721 collection", async () => {
+    xit("should be able to sell a single NFT into a bid for a single NFT", async () => {
       // bidder bids 0.000001 ETH for 1 BAYC
       await contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 1000000000000});
       // seller sells 1 BAYC into bid
@@ -80,15 +92,21 @@ contract("LiquidityFactory", (accounts) => {
       //assert.equal(result.logs[0].args.quantity, 1);
       //assert.equal(result.logs[0].args.tokenId, 250);
     });
+    xit("should throw if bid amount is less than seller expects", async () => {
+    });
     xit("should not be able to sell an ineligible NFT into a bid for a specific collection", async () => {
     });
     xit("should not be able to sell multiple NFTs into a bid for a single NFT", async () => {
+    });
+    xit("should not be able to sell N NFTs into a bid for M NFTs, where N>M", async () => {
     });
     xit("should be able to sell a single NFT into a bid for multiple NFTs", async () => {
     });
     xit("should be able to sell a single NFT into a bid for multiple NFTs, and subsequently sell additional NFTs into that bid", async () => {
     });
     xit("should not be able to sell into a bid that has been canceled", async () => {
+    });
+    xit("should not be able to sell into a bid that does not exist", async () => {
     });
   })
 
@@ -107,7 +125,7 @@ contract("LiquidityFactory", (accounts) => {
       // bidder is bidding 0 ETH for 1 Cryptopunk
       await utils.shouldThrow(contractInstance.submitBid(CRYPTOPUNK_ADDRESS, 1, {from: bidder, value: 0}));
     });
-    it("should be able to cancel bids", async () => {
+    it("should be able to cancel a bid", async () => {
       // bidder is bidding 0.1 ETH for 1 Cryptopunk
       await contractInstance.submitBid(CRYPTOPUNK_ADDRESS, 1, {from: bidder, value: 100000000000000000});
       // bidder is canceling Cryptopunk bid
@@ -116,6 +134,18 @@ contract("LiquidityFactory", (accounts) => {
       assert.equal(result.logs[0].args.bidderAddress, bidder);
       assert.equal(result.logs[0].args.nftAddress, CRYPTOPUNK_ADDRESS);
       assert.equal(result.logs[0].args.weiPriceEach, 100000000000000000);
+      assert.equal(result.logs[0].args.quantity, 1);
+      assert.equal(result.logs[0].args.tokenId, 0);
+    });
+    it("should be able to cancel multiple bids", async () => {
+      // bidder is bidding 0.2 ETH for 2 Cryptopunks
+      await contractInstance.submitBid(CRYPTOPUNK_ADDRESS, 2, {from: bidder, value: 200000000000000000});
+      // bidder is canceling Cryptopunk bid
+      const result = await contractInstance.cancelBid(CRYPTOPUNK_ADDRESS, {from: bidder});
+      assert.equal(result.receipt.status, true);
+      assert.equal(result.logs[0].args.bidderAddress, bidder);
+      assert.equal(result.logs[0].args.nftAddress, CRYPTOPUNK_ADDRESS);
+      assert.equal(result.logs[0].args.weiPriceEach, 200000000000000000);
       assert.equal(result.logs[0].args.quantity, 1);
       assert.equal(result.logs[0].args.tokenId, 0);
     });
@@ -145,18 +175,42 @@ contract("LiquidityFactory", (accounts) => {
     });
   })
 
-  context("as a seller, selling a Cryptopunk", async () => {
+  xcontext("as a seller, selling a Cryptopunk", async () => {
     xit("should be able to sell a single Cryptopunk into a bid for a single Cryptopunk", async () => {
+    });
+    xit("should throw if bid amount is less than seller expects", async () => {
     });
     xit("should not be able to sell an ineligible NFT into a bid for a Cryptopunk", async () => {
     });
     xit("should not be able to sell multiple Cryptopunks into a bid for a single Cryptopunk", async () => {
+    });
+    xit("should not be able to sell N Cryptopunks into a bid for M Cryptopunks, where N>M", async () => {
     });
     xit("should be able to sell a single Cryptopunk into a bid for multiple Cryptopunk", async () => {
     });
     xit("should be able to sell a single Cryptopunk into a bid for multiple Cryptopunks, and subsequently sell additional Cryptopunks into that bid", async () => {
     });
     xit("should not be able to sell a Cryptopunk into a bid that has been canceled", async () => {
+    });
+    xit("should not be able to sell into a bid that does not exist", async () => {
+    });
+  })
+
+  xcontext("as owner", async () => {
+    xit("should be able to invoke changePlatformFee", async () => {
+    });
+    xit("should receive correct payout for a single completed transaction", async () => {
+    });
+    xit("should receive correct payout for multiple completed transactions", async () => {
+    });
+    xit("should have unchanged balance when a new bid is placed", async () => {
+    });
+    xit("should have unchanged balance when a bid is canceled", async () => {
+    });
+  })
+
+  xcontext("as non-owner", async () => {
+    xit("should not be able to invoke changePlatformFee", async () => {
     });
   })
 

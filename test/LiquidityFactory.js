38de6ec2a9d81866ac1a7297589377d0d59eb1d7
kamilsadik <kamilsadik@gmail.com>
2022-01-03 10:30:53 -0500

    Refactored frequently used NFT contract addresses

--- test/LiquidityFactory.js
+++ test/LiquidityFactory.js
@@ -1,6 +1,9 @@
 const LiquidityFactory = artifacts.require("LiquidityFactory");
 const utils = require("./helpers/utils");
 
+const CRYPTOPUNK_ADDRESS = "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB";
+const BAYC_ADDRESS = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D";
+
 contract("LiquidityFactory", (accounts) => {
 
   let [owner, bidder, seller] = accounts;
@@ -12,53 +15,53 @@ contract("LiquidityFactory", (accounts) => {
   context("as a bidder, bidding on an ERC-721 collection", async () => {
     it("should be able to submit a bid", async () => {
         // bidder is bidding 0.1 ETH for 1 BAYC
-        const result = await contractInstance.submitBid("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", 1, {from: bidder, value: 100000000000000000});
+        const result = await contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 100000000000000000});
         assert.equal(result.receipt.status, true);
         assert.equal(result.logs[0].args.bidderAddress, bidder);
-        assert.equal(result.logs[0].args.nftAddress, "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D");
+        assert.equal(result.logs[0].args.nftAddress, BAYC_ADDRESS);
         assert.equal(result.logs[0].args.weiPriceEach, 100000000000000000);
         assert.equal(result.logs[0].args.quantity, 1);
         assert.equal(result.logs[0].args.tokenId, 0);
     });
-    it("should not be able to bid 0 ETH on a collection", async () => {
+    it("should not be able to bid 0 ETH", async () => {
         // bidder is bidding 0 ETH for 1 BAYC
-        await utils.shouldThrow(contractInstance.submitBid("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", 1, {from: bidder, value: 0}));
+        await utils.shouldThrow(contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 0}));
     });
     it("should be able to cancel bids previously submitted on a given collection", async () => {
         // bidder is bidding 0.1 ETH for 1 BAYC
-        await contractInstance.submitBid("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", 1, {from: bidder, value: 100000000000000000});
+        await contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 100000000000000000});
         // bidder is canceling BAYC bid
-        const result = await contractInstance.cancelBid("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", {from: bidder});
+        const result = await contractInstance.cancelBid(BAYC_ADDRESS, {from: bidder});
         assert.equal(result.receipt.status, true);
         assert.equal(result.logs[0].args.bidderAddress, bidder);
-        assert.equal(result.logs[0].args.nftAddress, "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D");
+        assert.equal(result.logs[0].args.nftAddress, BAYC_ADDRESS);
         assert.equal(result.logs[0].args.weiPriceEach, 100000000000000000);
         assert.equal(result.logs[0].args.quantity, 1);
         assert.equal(result.logs[0].args.tokenId, 0);
     });
     it("should not be able to place a bid on a collection where you already have a bid outstanding", async () => {
       // bidder is bidding 0.1 ETH for 1 BAYC
-      await contractInstance.submitBid("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", 1, {from: bidder, value: 100000000000000000});
+      await contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 100000000000000000});
       // bidder is trying to bid 0.1 ETH for 1 BAYC again
-      await utils.shouldThrow(contractInstance.submitBid("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", 1, {from: bidder, value: 100000000000000000}));
+      await utils.shouldThrow(contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 100000000000000000}));
     });
     it("should be able to place a bid, cancel a bid, and place a new bid on a collection", async () => {
       // bidder is bidding 0.1 ETH for 1 BAYC
-      await contractInstance.submitBid("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", 1, {from: bidder, value: 100000000000000000});
+      await contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 100000000000000000});
       // bidder is canceling BAYC bid
-      await contractInstance.cancelBid("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", {from: bidder});
+      await contractInstance.cancelBid(BAYC_ADDRESS, {from: bidder});
       // bidder is bidding 0.1 ETH for 1 BAYC
-      const result = await contractInstance.submitBid("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", 1, {from: bidder, value: 100000000000000000});
+      const result = await contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 100000000000000000});
       assert.equal(result.receipt.status, true);
       assert.equal(result.logs[0].args.bidderAddress, bidder);
-      assert.equal(result.logs[0].args.nftAddress, "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D");
+      assert.equal(result.logs[0].args.nftAddress, BAYC_ADDRESS);
       assert.equal(result.logs[0].args.weiPriceEach, 100000000000000000);
       assert.equal(result.logs[0].args.quantity, 1);
       assert.equal(result.logs[0].args.tokenId, 0);
     });
     it("should not be able to cancel a bid if you haven't already placed one", async () => {
       // bidder is trying to cancel a bid that does not exist
-      await utils.shouldThrow(contractInstance.cancelBid("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", {from: bidder}));
+      await utils.shouldThrow(contractInstance.cancelBid(BAYC_ADDRESS, {from: bidder}));
     });
   })
 
@@ -78,17 +81,55 @@ contract("LiquidityFactory", (accounts) => {
   })
 
   context("as a bidder, bidding on a Cryptopunk", async () => {
-    xit("should be able to submit a bid", async () => {
+    it("should be able to submit a bid", async () => {
+      // bidder is bidding 0.1 ETH for 1 Cryptopunk
+      const result = await contractInstance.submitBid(CRYPTOPUNK_ADDRESS, 1, {from: bidder, value: 100000000000000000});
+      assert.equal(result.receipt.status, true);
+      assert.equal(result.logs[0].args.bidderAddress, bidder);
+      assert.equal(result.logs[0].args.nftAddress, CRYPTOPUNK_ADDRESS);
+      assert.equal(result.logs[0].args.weiPriceEach, 100000000000000000);
+      assert.equal(result.logs[0].args.quantity, 1);
+      assert.equal(result.logs[0].args.tokenId, 0);
     });
-    xit("should not be able to bid 0 ETH on a collection", async () => {
+    it("should not be able to bid 0 ETH", async () => {
+      // bidder is bidding 0 ETH for 1 Cryptopunk
+      await utils.shouldThrow(contractInstance.submitBid(CRYPTOPUNK_ADDRESS, 1, {from: bidder, value: 0}));
     });
-    xit("should be able to cancel bids on a given collection", async () => {
+    it("should be able to cancel bids", async () => {
+      // bidder is bidding 0.1 ETH for 1 Cryptopunk
+      await contractInstance.submitBid(CRYPTOPUNK_ADDRESS, 1, {from: bidder, value: 100000000000000000});
+      // bidder is canceling Cryptopunk bid
+      const result = await contractInstance.cancelBid(CRYPTOPUNK_ADDRESS, {from: bidder});
+      assert.equal(result.receipt.status, true);
+      assert.equal(result.logs[0].args.bidderAddress, bidder);
+      assert.equal(result.logs[0].args.nftAddress, CRYPTOPUNK_ADDRESS);
+      assert.equal(result.logs[0].args.weiPriceEach, 100000000000000000);
+      assert.equal(result.logs[0].args.quantity, 1);
+      assert.equal(result.logs[0].args.tokenId, 0);
     });
-    xit("should not be able to place a bid on a collection where you already have a bid outstanding", async () => {
+    it("should not be able to place a bid if you already have a bid outstanding", async () => {
+      // bidder is bidding 0.1 ETH for 1 Cryptopunk
+      await contractInstance.submitBid(CRYPTOPUNK_ADDRESS, 1, {from: bidder, value: 100000000000000000});
+      // bidder is trying to bid 0.1 ETH for 1 Cryptopunk again
+      await utils.shouldThrow(contractInstance.submitBid(CRYPTOPUNK_ADDRESS, 1, {from: bidder, value: 100000000000000000}));
     });
-    xit("should be able to place a bid, cancel a bid, and place a new bid on a collection", async () => {
+    xit("should be able to place a bid, cancel a bid, and place a new bid", async () => {
+      // bidder is bidding 0.1 ETH for 1 Cryptopunk
+      await contractInstance.submitBid(CRYPTOPUNK_ADDRESS, 1, {from: bidder, value: 100000000000000000});
+      // bidder is canceling Cryptopunk bid
+      await contractInstance.cancelBid(CRYPTOPUNK_ADDRESS, {from: bidder});
+      // bidder is bidding 0.1 ETH for 1 Cryptopunk
+      const result = await contractInstance.submitBid(CRYPTOPUNK_ADDRESS, 1, {from: bidder, value: 100000000000000000});
+      assert.equal(result.receipt.status, true);
+      assert.equal(result.logs[0].args.bidderAddress, bidder);
+      assert.equal(result.logs[0].args.nftAddress, CRYPTOPUNK_ADDRESS);
+      assert.equal(result.logs[0].args.weiPriceEach, 100000000000000000);
+      assert.equal(result.logs[0].args.quantity, 1);
+      assert.equal(result.logs[0].args.tokenId, 0);
     });
-    xit("should not be able to cancel a bid if you haven't already placed one", async () => {
+    it("should not be able to cancel a bid if you haven't already placed one", async () => {
+      // bidder is trying to cancel a bid that does not exist
+      await utils.shouldThrow(contractInstance.cancelBid(CRYPTOPUNK_ADDRESS, {from: bidder}));
     });
   })
 

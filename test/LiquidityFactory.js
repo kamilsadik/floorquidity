6103fd8c4976da6bc1903fa90a4c9a6fb4c0839b
kamilsadik <kamilsadik@gmail.com>
2022-01-17 13:05:33 -0500

    Implemented all ERC-721 sale test

--- test/LiquidityFactory.js
+++ test/LiquidityFactory.js
@@ -73,7 +73,7 @@ contract("LiquidityFactory", (accounts) => {
     //cryptopunk = new ethers.Contract(CRYPTOPUNK_ADDRESS, CRYPTOPUNK_ABI, await provider.getSigner());
   });
 
-  context("as a bidder, bidding on an ERC-721 collection", async () => {
+  xcontext("as a bidder, bidding on an ERC-721 collection", async () => {
     it("should be able to submit a bid", async () => {
         // bidder is bidding 0.1 ETH for 1 BAYC
         const result = await contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 100000000000000000});
@@ -178,48 +178,79 @@ contract("LiquidityFactory", (accounts) => {
       await contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 1000000000000});
       // seller approves first BAYC for sale
       const BAYC_SIGNER = await ethers.getSigner(BAYC_HOLDER_ADDRESS);
-      // seller approves second BAYC for sale
       await ERC721_BAYC.connect(BAYC_SIGNER).approve(contractInstance.address, BAYC_HOLDINGS_FIVE[0], {gasLimit: 500000});
+      // seller approves second BAYC for sale
       await ERC721_BAYC.connect(BAYC_SIGNER).approve(contractInstance.address, BAYC_HOLDINGS_FIVE[1], {gasLimit: 500000});
       // seller tries to sell 2 BAYC into bid
-      await utils.shouldThrow(contractInstance.hitMultipleBids([bidder, bidder], BAYC_ADDRESS, BAYC_HOLDINGS_FIVE.slice(1), 1000000000000, {from: BAYC_HOLDER_ADDRESS}));
+      await utils.shouldThrow(contractInstance.hitMultipleBids([bidder, bidder], BAYC_ADDRESS, BAYC_HOLDINGS_FIVE.slice(0,2), [1000000000000, 1000000000000], {from: BAYC_HOLDER_ADDRESS}));
     });
-    xit("should be able to sell a single NFT into a bid for multiple NFTs", async () => {
+    it("should be able to sell a single NFT into a bid for multiple NFTs", async () => {
       // bidder bids 0.000001 ETH for 1 BAYC (good for up to 5 BAYC)
       await contractInstance.submitBid(BAYC_ADDRESS, 5, {from: bidder, value: 5000000000000});
+      // seller approves contract address to transfer BAYC
+      const BAYC_SIGNER = await ethers.getSigner(BAYC_HOLDER_ADDRESS);
+      await ERC721_BAYC.connect(BAYC_SIGNER).approve(contractInstance.address, BAYC_HOLDINGS_ONE, {gasLimit: 500000});
       // seller sells 1 BAYC into bid
-      const result = await contractInstance.hitBid(bidder, BAYC_ADDRESS, BAYC_HOLDINGS_ONE, 1000000000000, {from: BAYC_HOLDER});
+      const result = await contractInstance.hitBid(bidder, BAYC_ADDRESS, BAYC_HOLDINGS_ONE, 1000000000000, {from: BAYC_HOLDER_ADDRESS});
       assert.equal(result.receipt.status, true);
       assert.equal(result.logs[0].args.bidderAddress, bidder);
-      assert.equal(result.logs[0].args.sellerAddress, BAYC_HOLDER);
+      assert.equal(result.logs[0].args.sellerAddress, BAYC_HOLDER_ADDRESS);
       assert.equal(result.logs[0].args.nftAddress, BAYC_ADDRESS);
       assert.equal(result.logs[0].args.weiPriceEach, 1000000000000);
       assert.equal(result.logs[0].args.quantity, 1);
       assert.equal(result.logs[0].args.tokenId, BAYC_HOLDINGS_ONE);
     });
-    xit("should be able to sell N NFTs into a bid for M NFTs, where N<M", async () => {
+    it("should be able to sell N NFTs into a bid for M NFTs, where N<M", async () => {
       // bidder bids 0.000001 ETH for 1 BAYC (good for up to 5 BAYC)
       await contractInstance.submitBid(BAYC_ADDRESS, 5, {from: bidder, value: 5000000000000});
+      // seller approves first BAYC for sale
+      const BAYC_SIGNER = await ethers.getSigner(BAYC_HOLDER_ADDRESS);
+      await ERC721_BAYC.connect(BAYC_SIGNER).approve(contractInstance.address, BAYC_HOLDINGS_FIVE[0], {gasLimit: 500000});
+      // seller approves second BAYC for sale
+      await ERC721_BAYC.connect(BAYC_SIGNER).approve(contractInstance.address, BAYC_HOLDINGS_FIVE[1], {gasLimit: 500000});      
       // seller sells 2 BAYC into bid
-      const result = await contractInstance.hitMultipleBids([bidder, bidder], BAYC_ADDRESS, BAYC_HOLDINGS_FIVE.slice(1), 1000000000000, {from: BAYC_HOLDER});
+      const result = await contractInstance.hitMultipleBids([bidder, bidder], BAYC_ADDRESS, BAYC_HOLDINGS_FIVE.slice(0,2), [1000000000000, 1000000000000], {from: BAYC_HOLDER_ADDRESS});
       assert.equal(result.receipt.status, true);
       //TODO: other NewTrade event-related checks
     });
-    xit("should not be able to sell into a bid that has been canceled", async () => {
+    it("should be able to sell N_1 NFTs, then N_2 NFTs, into a bid for M NFTs, where N_1 + N_2 < M", async () => {
+      // bidder bids 0.000001 ETH for 1 BAYC (good for up to 5 BAYC)
+      await contractInstance.submitBid(BAYC_ADDRESS, 5, {from: bidder, value: 5000000000000});
+      // seller approves first and second BAYC for sale
+      const BAYC_SIGNER = await ethers.getSigner(BAYC_HOLDER_ADDRESS);
+      await ERC721_BAYC.connect(BAYC_SIGNER).approve(contractInstance.address, BAYC_HOLDINGS_FIVE[0], {gasLimit: 500000});
+      await ERC721_BAYC.connect(BAYC_SIGNER).approve(contractInstance.address, BAYC_HOLDINGS_FIVE[1], {gasLimit: 500000});
+      // seller sells first and second BAYC into bid
+      const result1 = await contractInstance.hitMultipleBids([bidder, bidder], BAYC_ADDRESS, BAYC_HOLDINGS_FIVE.slice(0,2), [1000000000000, 1000000000000], {from: BAYC_HOLDER_ADDRESS});
+      // seller approves third and fourth BAYC for sale
+      await ERC721_BAYC.connect(BAYC_SIGNER).approve(contractInstance.address, BAYC_HOLDINGS_FIVE[2], {gasLimit: 500000});
+      await ERC721_BAYC.connect(BAYC_SIGNER).approve(contractInstance.address, BAYC_HOLDINGS_FIVE[3], {gasLimit: 500000});
+      // seller sells third and fourth BAYC into bid
+      const result2 = await contractInstance.hitMultipleBids([bidder, bidder], BAYC_ADDRESS, BAYC_HOLDINGS_FIVE.slice(2,4), [1000000000000, 1000000000000], {from: BAYC_HOLDER_ADDRESS});
+      assert.equal(result1.receipt.status, true);
+      assert.equal(result2.receipt.status, true);
+    });
+    it("should not be able to sell into a bid that has been canceled", async () => {
       // bidder is bidding 0.000001 ETH for 1 BAYC
       await contractInstance.submitBid(BAYC_ADDRESS, 1, {from: bidder, value: 1000000000000});
       // bidder is canceling BAYC bid
       await contractInstance.cancelBid(BAYC_ADDRESS, {from: bidder});
+      // seller approves contract address to transfer BAYC
+      const BAYC_SIGNER = await ethers.getSigner(BAYC_HOLDER_ADDRESS);
+      await ERC721_BAYC.connect(BAYC_SIGNER).approve(contractInstance.address, BAYC_HOLDINGS_ONE, {gasLimit: 500000});
       // seller is trying to sell into canceled bid
-      await utils.shouldThrow(contractInstance.hitBid(bidder, BAYC_ADDRESS, BAYC_HOLDINGS_ONE, 1000000000000, {from: BAYC_HOLDER}));
+      await utils.shouldThrow(contractInstance.hitBid(bidder, BAYC_ADDRESS, BAYC_HOLDINGS_ONE, 1000000000000, {from: BAYC_HOLDER_ADDRESS}));
     });
-    xit("should not be able to sell into a bid that does not exist", async () => {
+    it("should not be able to sell into a bid that does not exist", async () => {
+      // seller approves contract address to transfer BAYC
+      const BAYC_SIGNER = await ethers.getSigner(BAYC_HOLDER_ADDRESS);
+      await ERC721_BAYC.connect(BAYC_SIGNER).approve(contractInstance.address, BAYC_HOLDINGS_ONE, {gasLimit: 500000});
       // seller is trying to sell into a nonexistent bid
-      await utils.shouldThrow(contractInstance.hitBid(bidder, BAYC_ADDRESS, BAYC_HOLDINGS_ONE, 1000000000000, {from: BAYC_HOLDER}));
+      await utils.shouldThrow(contractInstance.hitBid(bidder, BAYC_ADDRESS, BAYC_HOLDINGS_ONE, 1000000000000, {from: BAYC_HOLDER_ADDRESS}));
     });
   })
 
-  context("as a bidder, bidding on a Cryptopunk", async () => {
+  xcontext("as a bidder, bidding on a Cryptopunk", async () => {
     it("should be able to submit a bid", async () => {
       // bidder is bidding 0.1 ETH for 1 Cryptopunk
       const result = await contractInstance.submitBid(CRYPTOPUNK_ADDRESS, 1, {from: bidder, value: 100000000000000000});

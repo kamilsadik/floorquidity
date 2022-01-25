01fd3386ef3e3842c3c167aa9683ebf794cfd46a
kamilsadik <kamilsadik@gmail.com>
2022-01-25 17:15:52 -0500

    update

--- test/LiquidityFactory.js
+++ test/LiquidityFactory.js
@@ -424,7 +424,7 @@ contract("LiquidityFactory", (accounts) => {
     });
   })
 
-  xcontext("as owner", async () => {
+  context("as owner", async () => {
     it("should be able to invoke changePlatformFee", async () => {
       const result = await contractInstance.changePlatformFee(100, {from: owner});
       assert.equal(result.receipt.status, true);
